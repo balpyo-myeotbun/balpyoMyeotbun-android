8ea43144a813d668b2581dcaa07fdcf7631d829a
PIYUJIN <yjyj020201@gmail.com>
2024-03-20 01:24:25 +0900

    [#8] 발표시간 맞춤형 스크립트 생성 시간 결과 수정 화면 구성

--- app/src/main/java/com/project/balpyo/Script/ScriptResultFragment.kt
+++ app/src/main/java/com/project/balpyo/Script/ScriptResultFragment.kt
@@ -5,12 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getColorStateList
+import androidx.core.content.ContextCompat
 import com.project.balpyo.R
 import com.project.balpyo.databinding.FragmentScriptResultBinding
 
 class ScriptResultFragment : Fragment() {
 
     lateinit var binding: FragmentScriptResultBinding
+
+    var editable = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +24,21 @@ class ScriptResultFragment : Fragment() {
         binding = FragmentScriptResultBinding.inflate(layoutInflater)
 
         initToolBar()
+
+        binding.run {
+            buttonEdit.setOnClickListener {
+                if(editable) {
+                    buttonEdit.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.white)
+                    buttonEdit.text = "대본 수정하기"
+                    buttonEdit.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray3))
+                } else {
+                    buttonEdit.backgroundTintList = ContextCompat.getColorStateList(requireContext(), R.color.gray2)
+                    buttonEdit.text = "대본 수정 완료"
+                    buttonEdit.setTextColor(ContextCompat.getColor(requireContext(), R.color.gray4))
+                }
+                editable = !editable
+            }
+        }
 
         return binding.root
     }
