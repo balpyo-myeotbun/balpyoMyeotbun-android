652bd9e8f82f5914e0fb02994c04bc769d4f566f
PIYUJIN <yjyj020201@gmail.com>
2024-03-21 03:17:10 +0900

    [#12] 단어당 발표 시간 계산기(관리기) 빠르기 입력 화면 빠르기 조절시 오디오 출력 기능 구현

--- app/src/main/java/com/project/balpyo/TimeCalculator/TimeCalculatorSpeedFragment.kt
+++ app/src/main/java/com/project/balpyo/TimeCalculator/TimeCalculatorSpeedFragment.kt
@@ -1,12 +1,14 @@
 package com.project.balpyo.TimeCalculator
 
 import android.content.ContentValues.TAG
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.project.balpyo.R
 import com.project.balpyo.databinding.FragmentTimeCalculatorSpeedBinding
 import com.warkiz.widget.IndicatorSeekBar
 import com.warkiz.widget.OnSeekChangeListener
@@ -17,12 +19,24 @@ class TimeCalculatorSpeedFragment : Fragment() {
 
     lateinit var binding: FragmentTimeCalculatorSpeedBinding
 
+    lateinit var mediaPlayerMinusOne: MediaPlayer
+    lateinit var mediaPlayerMinusTwo: MediaPlayer
+    lateinit var mediaPlayerZero: MediaPlayer
+    lateinit var mediaPlayerOne: MediaPlayer
+    lateinit var mediaPlayerTwo: MediaPlayer
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
         binding = FragmentTimeCalculatorSpeedBinding.inflate(layoutInflater)
+
+        mediaPlayerMinusTwo = MediaPlayer.create(requireContext(), R.raw.minustwo)
+        mediaPlayerMinusOne = MediaPlayer.create(requireContext(), R.raw.minusone)
+        mediaPlayerZero = MediaPlayer.create(requireContext(), R.raw.zero)
+        mediaPlayerOne = MediaPlayer.create(requireContext(), R.raw.one)
+        mediaPlayerTwo = MediaPlayer.create(requireContext(), R.raw.two)
 
         initToolBar()
 
@@ -36,6 +50,51 @@ class TimeCalculatorSpeedFragment : Fragment() {
                     //when tick count > 0
                     Log.i(TAG, seekParams.thumbPosition.toString())
                     Log.i(TAG, seekParams.tickText)
+
+
+                    when(seekParams.tickText.toString()) {
+                        "-2" -> {
+                            mediaPlayerMinusTwo = MediaPlayer.create(requireContext(), R.raw.minustwo)
+                            mediaPlayerMinusOne.stop()
+                            mediaPlayerZero.stop()
+                            mediaPlayerOne.stop()
+                            mediaPlayerTwo.stop()
+                            mediaPlayerMinusTwo.start()
+                            Log.d("발표몇분","-2")
+                        }
+                        "-1" -> {
+                            mediaPlayerMinusOne = MediaPlayer.create(requireContext(), R.raw.minusone)
+                            mediaPlayerMinusTwo.stop()
+                            mediaPlayerZero.stop()
+                            mediaPlayerOne.stop()
+                            mediaPlayerTwo.stop()
+                            mediaPlayerMinusOne.start()
+                        }
+                        "0" -> {
+                            mediaPlayerZero = MediaPlayer.create(requireContext(), R.raw.zero)
+                            mediaPlayerMinusTwo.stop()
+                            mediaPlayerMinusOne.stop()
+                            mediaPlayerOne.stop()
+                            mediaPlayerTwo.stop()
+                            mediaPlayerZero.start()
+                        }
+                        "1" -> {
+                            mediaPlayerOne = MediaPlayer.create(requireContext(), R.raw.one)
+                            mediaPlayerMinusTwo.stop()
+                            mediaPlayerMinusOne.stop()
+                            mediaPlayerZero.stop()
+                            mediaPlayerTwo.stop()
+                            mediaPlayerOne.start()
+                        }
+                        "2" -> {
+                            mediaPlayerTwo = MediaPlayer.create(requireContext(), R.raw.two)
+                            mediaPlayerMinusTwo.stop()
+                            mediaPlayerMinusOne.stop()
+                            mediaPlayerZero.stop()
+                            mediaPlayerOne.stop()
+                            mediaPlayerTwo.start()
+                        }
+                    }
                 }
 
                 override fun onStartTrackingTouch(seekBar: IndicatorSeekBar) {}
