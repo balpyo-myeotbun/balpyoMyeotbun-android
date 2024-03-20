af733db179b8b1b641abeb35b3fa5336d0dc9bda
PIYUJIN <yjyj020201@gmail.com>
2024-03-21 02:23:15 +0900

    [#10] 기본 API 세팅

---
+++ app/src/main/java/com/project/balpyo/api/ApiService.kt
@@ -0,0 +1,11 @@
+package com.project.balpyo.api
+
+import retrofit2.Call
+import com.project.balpyo.api.request.GenerateScriptRequest
+import com.project.balpyo.api.response.GenerateScriptResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface ApiService {
+
+}
