cf4496bd8ed0c4f006e2a54d712ee6adb071f007
PIYUJIN <yjyj020201@gmail.com>
2024-03-20 20:58:56 +0900

    [#10] 발표 시간 맞춤형 스크립트 생성 주제 입력 화면 기능 구현

---
+++ app/src/main/java/com/project/balpyo/Utils/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.project.balpyo.Utils
+
+import android.app.Application
+
+class MyApplication : Application() {
+    companion object {
+
+        var scriptSubject = ""
+        var scriptSubtopic = ""
+    }
+}
