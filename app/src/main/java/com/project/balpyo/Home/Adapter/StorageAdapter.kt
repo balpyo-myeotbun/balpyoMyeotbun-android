a31ae0588058057a1529c5e70079e6dd9834c072
PIYUJIN <yjyj020201@gmail.com>
2024-03-23 23:31:53 +0900

    [#33] 보관함 화면 수정

--- app/src/main/java/com/project/balpyo/Home/Adapter/StorageAdapter.kt
+++ app/src/main/java/com/project/balpyo/Home/Adapter/StorageAdapter.kt
@@ -21,8 +21,7 @@ class StorageAdapter () :
         fun onItemClick(position: Int) {}
     }
 
-    var itemClickListener1: OnItemClickListener? = null
-    var itemClickListener2: OnItemClickListener? = null
+    var itemClickListener: OnItemClickListener? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         context = parent.context
@@ -34,7 +33,6 @@ class StorageAdapter () :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.title.text = "인공지능의 역사"
-        holder.menu.text = "발표 시간 맞춤형 대본 생성"
     }
 
     override fun getItemCount() = 5
@@ -43,16 +41,11 @@ class StorageAdapter () :
     inner class ViewHolder(val binding: RowStorageBinding) :
         RecyclerView.ViewHolder(binding.root) {
         val title = binding.textViewScriptTitle
-        val menu = binding.textViewMenu
+        val player = binding.imageViewPlay
 
         init {
-            binding.buttonEdit.setOnClickListener {
-                itemClickListener1?.onItemClick(adapterPosition)
-                true
-            }
-
-            binding.buttonDelete.setOnClickListener {
-                itemClickListener2?.onItemClick(adapterPosition)
+            binding.root.setOnClickListener {
+                itemClickListener?.onItemClick(adapterPosition)
                 true
             }
         }
