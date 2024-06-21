b790f1cc8ae99d284ed93b562fdb430414a125fe
Jihyeon <zz64446949@gmail.com>
2024-06-21 16:30:10 +0900

    REFACTOR: 스피드 조절 UI 수정 및 리팩토링

--- app/src/main/java/com/project/balpyo/FlowController/FlowControllerSpeedFragment.kt
+++ app/src/main/java/com/project/balpyo/FlowController/FlowControllerSpeedFragment.kt
@@ -1,23 +1,21 @@
 package com.project.balpyo.FlowController
 
-import android.content.ContentValues.TAG
 import android.content.Context
+import android.graphics.drawable.AnimationDrawable
 import android.media.MediaPlayer
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.project.balpyo.FlowController.ViewModel.FlowControllerViewModel
 import com.project.balpyo.R
 import com.project.balpyo.databinding.FragmentFlowControllerSpeedBinding
-import com.warkiz.widget.IndicatorSeekBar
-import com.warkiz.widget.OnSeekChangeListener
-import com.warkiz.widget.SeekParams
 
 
 class FlowControllerSpeedFragment : Fragment() {
@@ -26,11 +24,12 @@ class FlowControllerSpeedFragment : Fragment() {
     private lateinit var flowControllerViewModel: FlowControllerViewModel
     private lateinit var callback: OnBackPressedCallback
 
-    lateinit var mediaPlayerMinusOne: MediaPlayer
-    lateinit var mediaPlayerMinusTwo: MediaPlayer
-    lateinit var mediaPlayerZero: MediaPlayer
-    lateinit var mediaPlayerOne: MediaPlayer
-    lateinit var mediaPlayerTwo: MediaPlayer
+    private val mediaPlayers: Array<MediaPlayer?> = arrayOfNulls(5)
+
+    private val CLs: Array<ConstraintLayout?> = arrayOfNulls(5)
+    private val TVs: Array<TextView?> = arrayOfNulls(5)
+
+    private lateinit var animationDrawable: AnimationDrawable
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,87 +39,83 @@ class FlowControllerSpeedFragment : Fragment() {
         binding = FragmentFlowControllerSpeedBinding.inflate(layoutInflater)
         flowControllerViewModel = ViewModelProvider(requireActivity())[FlowControllerViewModel::class.java]
 
-        mediaPlayerMinusTwo = MediaPlayer.create(requireContext(), R.raw.minustwo)
-        mediaPlayerMinusOne = MediaPlayer.create(requireContext(), R.raw.minusone)
-        mediaPlayerZero = MediaPlayer.create(requireContext(), R.raw.zero)
-        mediaPlayerOne = MediaPlayer.create(requireContext(), R.raw.one)
-        mediaPlayerTwo = MediaPlayer.create(requireContext(), R.raw.two)
+        animationDrawable = binding.FCIV.drawable as AnimationDrawable
 
         initToolBar()
+        initailizeMediaPlayers()
+        initailizeCLsAndTVs()
+
+        flowControllerViewModel.setSpeed(0)
+        startPlayer(0, R.raw.zero)
+        setSpeedColor(0)
+
+        setSpeedBtnClickListeners()
 
         binding.run {
             buttonNext.setOnClickListener {
                 stopMediaPlayers()
                 findNavController().navigate(R.id.flowControllerPreviewFragment)
             }
-            seekbar.setOnSeekChangeListener(object : OnSeekChangeListener {
-                override fun onSeeking(seekParams: SeekParams) {
-                    Log.i(TAG, seekParams.seekBar.toString())
-                    Log.i(TAG, seekParams.progress.toString())
-                    Log.i(TAG, seekParams.progressFloat.toString())
-                    Log.i(TAG, seekParams.fromUser.toString())
-                    //when tick count > 0
-                    Log.i(TAG, seekParams.thumbPosition.toString())
-                    Log.i(TAG, seekParams.tickText)
-
-
-                    when(seekParams.tickText.toString()) {
-                        "-2" -> {
-                            mediaPlayerMinusTwo = MediaPlayer.create(requireContext(), R.raw.minustwo)
-                            mediaPlayerMinusOne.stop()
-                            mediaPlayerZero.stop()
-                            mediaPlayerOne.stop()
-                            mediaPlayerTwo.stop()
-                            mediaPlayerMinusTwo.start()
-                            Log.d("발표몇분","-2")
-                            flowControllerViewModel.setSpeed(-2)
-                        }
-                        "-1" -> {
-                            mediaPlayerMinusOne = MediaPlayer.create(requireContext(), R.raw.minusone)
-                            mediaPlayerMinusTwo.stop()
-                            mediaPlayerZero.stop()
-                            mediaPlayerOne.stop()
-                            mediaPlayerTwo.stop()
-                            mediaPlayerMinusOne.start()
-                            flowControllerViewModel.setSpeed(-1)
-                        }
-                        "0" -> {
-                            mediaPlayerZero = MediaPlayer.create(requireContext(), R.raw.zero)
-                            mediaPlayerMinusTwo.stop()
-                            mediaPlayerMinusOne.stop()
-                            mediaPlayerOne.stop()
-                            mediaPlayerTwo.stop()
-                            mediaPlayerZero.start()
-                            flowControllerViewModel.setSpeed(0)
-                        }
-                        "1" -> {
-                            mediaPlayerOne = MediaPlayer.create(requireContext(), R.raw.one)
-                            mediaPlayerMinusTwo.stop()
-                            mediaPlayerMinusOne.stop()
-                            mediaPlayerZero.stop()
-                            mediaPlayerTwo.stop()
-                            mediaPlayerOne.start()
-                            flowControllerViewModel.setSpeed(1)
-                        }
-                        "2" -> {
-                            mediaPlayerTwo = MediaPlayer.create(requireContext(), R.raw.two)
-                            mediaPlayerMinusTwo.stop()
-                            mediaPlayerMinusOne.stop()
-                            mediaPlayerZero.stop()
-                            mediaPlayerOne.stop()
-                            mediaPlayerTwo.start()
-                            flowControllerViewModel.setSpeed(2)
-                        }
-                    }
-                }
-
-                override fun onStartTrackingTouch(seekBar: IndicatorSeekBar) {}
-                override fun onStopTrackingTouch(seekBar: IndicatorSeekBar) {}
-            })
         }
         return binding.root
     }
 
+    fun initailizeMediaPlayers(){
+        mediaPlayers[0] = MediaPlayer.create(requireContext(), R.raw.minustwo)
+        mediaPlayers[1] = MediaPlayer.create(requireContext(), R.raw.minusone)
+        mediaPlayers[2] = MediaPlayer.create(requireContext(), R.raw.zero)
+        mediaPlayers[3] = MediaPlayer.create(requireContext(), R.raw.one)
+        mediaPlayers[4] = MediaPlayer.create(requireContext(), R.raw.two)
+    }
+
+    fun initailizeCLsAndTVs(){
+        binding.run {
+            CLs[0] = FCSpeedCL03
+            CLs[1] = FCSpeedCL05
+            CLs[2] = FCSpeedCL1
+            CLs[3] = FCSpeedCL15
+            CLs[4] = FCSpeedCL2
+
+            TVs[0] = FCSpeed03Tv
+            TVs[1] = FCSpeed05Tv
+            TVs[2] = FCSpeed1Tv
+            TVs[3] = FCSpeed15Tv
+            TVs[4] = FCSpeed2Tv
+        }
+    }
+
+    private fun setSpeedBtnClickListeners() {
+        val speeds = listOf(-2, -1, 0, 1, 2)
+        val rawResources = listOf(R.raw.minustwo, R.raw.minusone, R.raw.zero, R.raw.one, R.raw.two)
+        val layouts = CLs
+
+        for (i in speeds.indices) {
+            layouts[i]!!.setOnClickListener {
+                startPlayer(speeds[i], rawResources[i])
+                setSpeedColor(speeds[i])
+                flowControllerViewModel.setSpeed(speeds[i])
+            }
+        }
+    }
+
+    fun startPlayer(speed: Int, raw: Int){
+        stopMediaPlayers()
+        mediaPlayers[speed+2] = MediaPlayer.create(requireContext(), raw)
+        mediaPlayers[speed+2]!!.start()
+    }
+    fun setSpeedColor(speed : Int){
+        val selectedBg= R.drawable.selected_speed
+        val unselectedBg = R.drawable.unselected_speed_icon
+        val selectColor = resources.getColor(R.color.primary)
+        val unselectedColor = resources.getColor(R.color.disabled)
+
+        for(i in 0..4){
+            CLs[i]!!.setBackgroundResource(unselectedBg)
+            TVs[i]!!.setTextColor(unselectedColor)
+        }
+        CLs[speed + 2]!!.setBackgroundResource(selectedBg)
+        TVs[speed + 2]!!.setTextColor(selectColor)
+    }
     fun initToolBar() {
         binding.run {
             toolbar.buttonBack.visibility = View.VISIBLE
@@ -138,11 +133,24 @@ class FlowControllerSpeedFragment : Fragment() {
         }
     }
     private fun stopMediaPlayers() {
-        mediaPlayerMinusTwo.stop()
-        mediaPlayerMinusOne.stop()
-        mediaPlayerZero.stop()
-        mediaPlayerOne.stop()
-        mediaPlayerTwo.stop()
+        for (player in mediaPlayers) {
+                player?.stop()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        animationDrawable.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        animationDrawable.stop()
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        callback.remove()
     }
 
     //기기의 뒤로가기 버튼을 누를 시
@@ -155,10 +163,5 @@ class FlowControllerSpeedFragment : Fragment() {
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(this, callback)
-    }
-
-    override fun onDetach() {
-        super.onDetach()
-        callback.remove()
     }
 }
