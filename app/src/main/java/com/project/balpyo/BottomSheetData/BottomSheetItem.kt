6b80abb56ce89e53d2f4ed85e7322339329b968c
Jihyeon <zz64446949@gmail.com>
2024-06-26 15:25:43 +0900

    [#63] STYLE : 바텀 시트 UI 변경사항 적용 및 태그 추가

--- app/src/main/java/com/project/balpyo/BottomSheetData/BottomSheetItem.kt
+++ app/src/main/java/com/project/balpyo/BottomSheetData/BottomSheetItem.kt
@@ -1,3 +1,3 @@
 package com.project.balpyo.BottomSheetData
 
-data class BottomSheetItem(val title : String)
+data class BottomSheetItem(val title : String, val content : String = "", val timeStamp : String = "")
