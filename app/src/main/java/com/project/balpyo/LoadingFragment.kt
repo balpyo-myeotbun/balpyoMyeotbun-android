ff6a43767779de030cb923e8c7c8af4ed4a84e94
Jihyeon <zz64446949@gmail.com>
2024-06-30 18:27:16 +0900

    [#65] STYLE: 로딩 프래그먼트 UI 변경

--- app/src/main/java/com/project/balpyo/LoadingFragment.kt
+++ app/src/main/java/com/project/balpyo/LoadingFragment.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
 import com.project.balpyo.databinding.FragmentLoadingBinding
 import kotlin.concurrent.timer
@@ -15,8 +17,7 @@ import kotlin.concurrent.timer
 class LoadingFragment : Fragment() {
 
     lateinit var binding: FragmentLoadingBinding
-
-    var commentList = mutableListOf("발표 시간을 계산하고 있어요", "사람들의 이목을 끌 수 있는 발표대본을\n열심히 만들고 있어요", "곧 만들어져요!", "원하시는 시간에 최적화된 대본을 만들어드릴게요", "발표몇분이 열심히 대본을 만들고 있어요")
+    private val args: LoadingFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,27 +25,24 @@ class LoadingFragment : Fragment() {
     ): View? {
 
         binding = FragmentLoadingBinding.inflate(layoutInflater)
+        val toolbarTitle = args.toolbarTitle
+        val comment = args.comment
 
         binding.run {
             Glide.with(requireContext()).load(R.raw.loading).into(imageViewLoading)
-
-            val interval = 3000L // 밀리초 단위로 3초 설정
-
-            var i = 0
-
-            val handler = Handler(Looper.getMainLooper())
-            val runnable = object : Runnable {
-                override fun run() {
-                    textViewLoading.text = commentList[i]
-                    i++
-                    if (i >= commentList.size) i = 0 // i가 리스트 크기를 초과하면 다시 처음부터 시작
-                    handler.postDelayed(this, interval)
-                }
-            }
-
-            handler.post(runnable)
+            toolbar.textViewTitle.text = toolbarTitle
+            textViewSubLoading.setText(comment)
         }
 
         return binding.root
+    }
+
+    fun initToolBar() {
+        binding.run {
+            toolbar.buttonBack.visibility = View.INVISIBLE
+            toolbar.buttonClose.visibility = View.INVISIBLE
+            toolbar.textViewPage.visibility = View.INVISIBLE
+            toolbar.textViewTitle.visibility = View.VISIBLE
+        }
     }
 }
