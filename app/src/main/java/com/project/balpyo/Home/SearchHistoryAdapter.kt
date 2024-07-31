e63cc328cba4046bf3e82dc0ffbace01a7d0a8fc
Jihyeon <zz64446949@gmail.com>
2024-07-31 10:19:36 +0900

    [#85] Feat: 보관함 최근 검색 내역

---
+++ app/src/main/java/com/project/balpyo/Home/SearchHistoryAdapter.kt
@@ -0,0 +1,39 @@
+package com.project.balpyo.Home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.project.balpyo.databinding.ItemSearchHistoryBinding
+
+class SearchHistoryAdapter(private val items: List<String>,
+                           private val itemClickListener: (String) -> Unit,
+                           private val deleteClickListener: (String) -> Unit) :
+    RecyclerView.Adapter<SearchHistoryAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding =
+            ItemSearchHistoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.searchText.text = items[position]
+    }
+
+    override fun getItemCount() = items.size
+
+    inner class ViewHolder(val binding: ItemSearchHistoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val searchText = binding.tvItemSearchHistory
+        val deleteButton = binding.ivItemSearchHistory
+        init {
+            searchText.setOnClickListener {
+                itemClickListener(items[adapterPosition])
+            }
+            deleteButton.setOnClickListener {
+                deleteClickListener(items[adapterPosition])
+            }
+        }
+    }
+}
