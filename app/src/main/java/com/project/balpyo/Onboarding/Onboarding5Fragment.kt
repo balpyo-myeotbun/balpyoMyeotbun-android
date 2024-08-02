72c89d9a32640f4105c5cd98288c7688dc047cf9
PIYUJIN <yjyj020201@gmail.com>
2024-08-03 03:56:28 +0900

    [#93] 온보딩 화면 분기 처리 기능 구현

--- app/src/main/java/com/project/balpyo/Onboarding/Onboarding5Fragment.kt
+++ app/src/main/java/com/project/balpyo/Onboarding/Onboarding5Fragment.kt
@@ -6,12 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.project.balpyo.MainActivity
 import com.project.balpyo.R
+import com.project.balpyo.Utils.MyApplication
+import com.project.balpyo.Utils.PreferenceUtil
 import com.project.balpyo.databinding.FragmentOnboarding1Binding
 import com.project.balpyo.databinding.FragmentOnboarding5Binding
 
 class Onboarding5Fragment : Fragment() {
     lateinit var binding: FragmentOnboarding5Binding
+    lateinit var mainActivity: MainActivity
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,9 +23,13 @@ class Onboarding5Fragment : Fragment() {
     ): View? {
 
         binding = FragmentOnboarding5Binding.inflate(layoutInflater)
+        mainActivity = activity as MainActivity
+
+        MyApplication.preferences = PreferenceUtil(mainActivity)
 
         binding.run {
             buttonStart.setOnClickListener {
+                MyApplication.preferences.setOnBoarding("isFirst", false)
                 findNavController().navigate(R.id.loginFragment)
             }
         }
